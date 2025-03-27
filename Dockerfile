9e1dbe4a2c2e8a3899b2aea449ce3e3c4b748a8f
Sahil Malhotra <smalhotra@mitre.org>
2025-03-27 08:38:00 -0400

    spl updates

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:21-alpine
+WORKDIR /home/node/app/rems-directory
+
+COPY --chown=node:node . .
+RUN npm install
+EXPOSE 3323
+
+HEALTHCHECK --interval=30s --start-period=15s --timeout=10m --retries=10 CMD wget --no-verbose --tries=1 --spider http://localhost:33333/health || exit 1
+CMD npm run start
--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,13 @@
+import * as env from 'env-var';
+
 export type Medication = {
     brand_name: string;
     generic_name: string,
     product_ndc: string,
     rems_administrator: string,
-    rems_endpoint: string,
+    rems_endpoint: string | undefined,
     rems_approval_date: string,
+    rems_spl_date?: string
     rems_modification_date: string
 }
 
@@ -13,32 +16,34 @@ export const medications : Medication[] = [
         brand_name: "ADDYI",
         generic_name: "FLIBANSERINE",
         product_ndc: "58604-214",
-        rems_administrator: "REMS Prototype",
-        rems_endpoint: "http://localhost:3003/",
+        rems_administrator: "REMS Prototype Admin 1",
+        rems_endpoint: env.get('REMS_ADMIN_1_URL').asString(),
         rems_approval_date: "20240906",
         rems_modification_date: "20240906"
     }, {
         brand_name: "Isotretinoin",
         generic_name: "ISOTRETINOIN",
-        product_ndc: "0591-2433",
-        rems_administrator: "REMS Prototype",
-        rems_endpoint: "http://localhost:3003/",
+        product_ndc: "70771-1557-4",
+        rems_administrator: "REMS Prototype Admin 2",
+        rems_endpoint: env.get('REMS_ADMIN_2_URL').asString(),
         rems_approval_date: "20240906",
-        rems_modification_date: "20240906"
+        rems_modification_date: "20240906",
+        rems_spl_date: "20230912"
     }, {
         brand_name: "Fentanyl Citrate",
         generic_name: "FENTANYL CITRATE",
-        product_ndc: "0093-7865",
-        rems_administrator: "REMS Prototype",
-        rems_endpoint: "http://localhost:3003/",
+        product_ndc: "63459-502-01",
+        rems_administrator: "REMS Prototype Admin 1",
+        rems_endpoint: env.get('REMS_ADMIN_1_URL').asString(),
         rems_approval_date: "20240906",
-        rems_modification_date: "20240906"
+        rems_modification_date: "20240906",
+        rems_spl_date: "20230401"
     }, {
         brand_name: "Turalio",
         generic_name: "PEXIDARTINIB HYDROCHLORIDE",
-        product_ndc: "65597-407",
-        rems_administrator: "REMS Prototype",
-        rems_endpoint: "http://localhost:3003/",
+        product_ndc: "65597-407-20",
+        rems_administrator: "REMS Prototype Admin 2",
+        rems_endpoint: env.get('REMS_ADMIN_2_URL').asString(),
         rems_approval_date: "20240906",
         rems_modification_date: "20240906"
     } 
